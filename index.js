c52eb7d5196c77c85263b6f68982cc7d64d5f6be
Ayush Ninawe <66631743+AyushNinawe@users.noreply.github.com>
2025-04-23 17:45:31 +0530

    edit2

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoute from "./routes/userRoute.js"
 import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
-import tweetRoute from "./routes/tweetRoute.js"
+import userRoute from "./routes/userRoute.js";
+import tweetRoute from "./routes/tweetRoute.js";
 import cors from "cors";
 
-dotenv.config({
-    path:".env"
-})
-const app =express();
+dotenv.config({ path: ".env" });
+const port = process.env.PORT || 4000;
+// Connect to DB
+databaseConnection();
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://serene-daffodil-5eaf62.netlify.app'];
+  'http://localhost:3000',
+  'https://serene-daffodil-5eaf62.netlify.app',
+  'https://incandescent-selkie-a51894.netlify.app'
+];
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true
 }));
-app.use(cookieParser()); // ✅ Middleware to read cookies
-app.use(express.json()); // Parses JSON data
-// databaseConnection();
-//
-// const corsOptions={
-//     origin:"http://localhost:3000",
-//     credentials:true
-// }
-// app.use(cors(corsOptions));
-
-// app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
 
 
-// app.use((req,res,next));
-
-app.use((req,res,next)=>{
-    console.log("Request Cookies:",req.cookies);
-    console.log("Request Headers:",req.headers.authorization);
-    next();
+app.get('/',(req,res)=>{
+  res.send("Welcome to the API")
 })
 
+// Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/tweet", tweetRoute);
 
-app.use(express.urlencoded({
-    extended:true
-}));
-app.listen(process.env.PORT,()=>{
-    console.log(`server listen at port ${process.env.PORT}`);
-})
-app.get("/",(req,res)=>{
-    res.send("welcome to the Api ");
-
+// Start Server
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
 });
-
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/tweet",tweetRoute);
